b8954ce3a9381b243be6e4a556298232207474a1
jicase <jcaso@protonmail.com>
2020-03-21 14:50:15 +0100

    fix: fix gitignore

---
+++ src/visualization/flask/app.py
@@ -0,0 +1,19 @@
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
