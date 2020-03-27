b20c55459aa2a1a7016203751659917b9097a731
jicase <jcaso@protonmail.com>
2020-03-27 21:26:10 +0100

    feat: receive data from react OK

--- src/visualization/flask/app.py
+++ src/visualization/flask/app.py
@@ -1,12 +1,28 @@
 import time
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CORS(app, support_credentials=True)
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/api/query', methods=['POST'])
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+def get_query_from_react():
+    print("hello data")
+
+    print(request.get_json())
+
+    return "OK"
 
 
 @app.route('/')
@@ -15,4 +31,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
